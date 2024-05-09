a47b036e030a6535f98faefce3658d82309c6d78
HoTomy <134215338+HoTomy@users.noreply.github.com>
2024-05-09 05:59:59 +0000

    config

--- helpers/database.ts
+++ helpers/database.ts
@@ -1,6 +1,16 @@
 import { Sequelize, QueryTypes } from 'sequelize';
 //import { config } from "../common/config";
 
+const config = {
+ host: 'tiny.db.elephantsql.com',
+ port: 5432,
+ user:'apzvtefj',
+ password: 'NyiReeL2m-RoO-J_LUFDhneQR-ZnrZEb',
+ database: 'apzvtefj',
+ connection_limit: 100
+}
+
+
 export const run_query = async (query: string, values: any) => {
   try {
     const sequelize = new Sequelize(config.database, config.user, config.password, {
