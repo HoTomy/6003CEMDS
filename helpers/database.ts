47fd83a4e2c5b8d1164dda587e713c2967e5fd2d
HoTomy <134215338+HoTomy@users.noreply.github.com>
2024-05-09 10:14:41 +0000

    filter

--- helpers/database.ts
+++ helpers/database.ts
@@ -1,92 +1,111 @@
-import { Sequelize, QueryTypes } from 'sequelize';
+import { Sequelize, QueryTypes } from "sequelize";
 //import { config } from "../common/config";
 
 const config = {
- host: 'tiny.db.elephantsql.com',
- port: 5432,
- user:'apzvtefj',
- password: 'NyiReeL2m-RoO-J_LUFDhneQR-ZnrZEb',
- database: 'apzvtefj',
- connection_limit: 100
-}
-
+  host: "tiny.db.elephantsql.com",
+  port: 5432,
+  user: "apzvtefj",
+  password: "NyiReeL2m-RoO-J_LUFDhneQR-ZnrZEb",
+  database: "apzvtefj",
+  connection_limit: 100,
+};
 
 export const run_query = async (query: string, values: any) => {
   try {
-    const sequelize = new Sequelize(config.database, config.user, config.password, {
-      host: config.host,
-      port: config.port,
-      dialect: 'mysql'
-    });
+    const sequelize = new Sequelize(
+      config.database,
+      config.user,
+      config.password,
+      {
+        host: config.host,
+        port: config.port,
+        dialect: "mysql",
+      },
+    );
     await sequelize.authenticate();
     const data = await sequelize.query(query, {
       replacements: values,
-      type: QueryTypes.SELECT
+      type: QueryTypes.SELECT,
     });
     await sequelize.close();
     return data;
   } catch (err: any) {
     console.error(err, query, values);
-    throw 'Database query error';
+    throw "Database query error";
   }
-}
+};
 
-export const run_insertrecord = async (sql: string, values: any) => {
+export const run_insert = async (sql: string, values: any) => {
   try {
-    const sequelize = new Sequelize(config.database, config.user, config.password, {
-      host: config.host,
-      port: config.port,
-      dialect: 'mysql'
-    });
+    const sequelize = new Sequelize(
+      config.database,
+      config.user,
+      config.password,
+      {
+        host: config.host,
+        port: config.port,
+        dialect: "mysql",
+      },
+    );
     await sequelize.authenticate();
     const data = await sequelize.query(sql, {
       replacements: values,
-      type: QueryTypes.INSERT
+      type: QueryTypes.INSERT,
     });
     await sequelize.close();
     return data;
   } catch (err: any) {
     console.error(err, sql, values);
-    throw 'Database record insert error';
+    throw "Database record insert error";
   }
-}
+};
 
-export const run_updaterecord = async (sql: string, values: any) => {
+export const run_update = async (sql: string, values: any) => {
   try {
-    const sequelize = new Sequelize(config.database, config.user, config.password, {
-      host: config.host,
-      port: config.port,
-      dialect: 'mysql'
-    });
+    const sequelize = new Sequelize(
+      config.database,
+      config.user,
+      config.password,
+      {
+        host: config.host,
+        port: config.port,
+        dialect: "mysql",
+      },
+    );
     await sequelize.authenticate();
     const data = await sequelize.query(sql, {
       replacements: values,
-      type: QueryTypes.UPDATE
+      type: QueryTypes.UPDATE,
     });
     await sequelize.close();
     return data;
   } catch (err: any) {
     console.error(err, sql, values);
-    throw 'Database update record error';
+    throw "Database update record error";
   }
-}
+};
 
-export const run_deleterecord = async (sql: string, values: any) => {
+export const run_delete = async (sql: string, values: any) => {
   try {
-    const sequelize = new Sequelize(config.database, config.user, config.password, {
-      host: config.host,
-      port: config.port,
-      dialect: 'mysql'
-    });
+    const sequelize = new Sequelize(
+      config.database,
+      config.user,
+      config.password,
+      {
+        host: config.host,
+        port: config.port,
+        dialect: "mysql",
+      },
+    );
     await sequelize.authenticate();
     const data = await sequelize.query(sql, {
       replacements: values,
-      type: QueryTypes.DELETE
+      type: QueryTypes.DELETE,
     });
     await sequelize.close();
     return data;
   } catch (err: any) {
     console.error(err, sql, values);
-    throw 'Database delete record error';
+    throw "Database delete record error";
   }
-}
+};
